cf6b5b274c1243a0f8947d50b8ccb293543a1663
sahabatdavid <hellosahabatdavid@gmail.com>
2020-11-08 18:30:11 +0700

    Add Model and Seeder

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'role_id'
     ];
 
     /**
@@ -36,4 +36,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function hasPermission($permission) {
+        return $this->role->permissions()->where('name', $permission)->first() ?: false;
+    }
 }
